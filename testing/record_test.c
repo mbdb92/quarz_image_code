2374e455e4dd5894c697217f89650de014971e40
nithor <md@eefd.de>
2019-11-15 10:23:23 +0100

    changed recorder code to write to file

--- testing/record_test.c
+++ testing/record_test.c
@@ -1,6 +1,12 @@
 #define ALSA_PCM_NEW_HW_PARAMS_API
 
 #include <alsa/asoundlib.h>
+// For time function
+#include <time.h>
+// for file access
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main () {
     long loops;
@@ -12,7 +18,16 @@ int main () {
     int dir;
     snd_pcm_uframes_t frames;
     char *buffer;
+    clock_t t;
+    int fd;
 
+    fd = open("record.raw", O_WRONLY | O_CREAT);
+    if (fd == -1) {
+        fprintf(stderr, "unable to open file: %s\n", strerror(errno));
+        exit(4);
+    }
+
+    t = clock();
     rc = snd_pcm_open(&handle, "default", SND_PCM_STREAM_CAPTURE, 0);
 
     if (rc < 0){
@@ -28,17 +43,26 @@ int main () {
     snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir);
     frames = 32;
     snd_pcm_hw_params_set_period_size_near(handle, params, &frames, &dir);
+    
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    printf("setup took %f seconds to execute \n", time_taken); 
 
     rc = snd_pcm_hw_params(handle, params);
     if (rc < 0) {
         fprintf (stderr, "unable to set hw parameters: %s\n", snd_strerror(rc));
         exit(2);
     }
+
+    t = clock() - t;
+    time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    printf("writing setu took %f seconds to execute \n", time_taken); 
+
     snd_pcm_hw_params_get_period_size(params, &frames, &dir);
     size = frames * 4;
     buffer = (char *) malloc(size);
     snd_pcm_hw_params_get_period_time(params, &val, &dir);
-    loops = 5000000/ val;
+    loops = 1000;
     while (loops > 0) {
         loops--;
         rc = snd_pcm_readi(handle, buffer, frames);
@@ -50,14 +74,20 @@ int main () {
         }else if (rc != (int)frames) {
             fprintf(stderr, "short read, read %d frames\n", rc);
         }
-        rc = write(1, buffer, size);
+        rc = write(fd, buffer, size);
         if (rc != size) {
             fprintf(stderr, "short write: wrote %d bytes\n", rc);
         }
+
+        t = clock() - t;
+        time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+        printf("%f seconds\n", time_taken); 
+
     }
 
     snd_pcm_drain(handle);
     snd_pcm_close(handle);
+    close(fd);
     free(buffer);
 
     return 0;
