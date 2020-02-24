85c8fe9eedb3e05af064c3c051cbd1b2d0ce58ec
nithor <md@eefd.de>
2020-02-25 00:33:31 +0100

    Imagemagick

---
+++ imagemagic/image_test.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+//#include <MagickCore/MagickCore.h>
+#include <MagickWand/MagickWand.h>
+
+int main() {
+    /*
+    ExceptionInfo *exception;
+    Image *image;
+    ImageInfo *image_info;
+    int pixels[25];
+    MagickBooleanType returnval;
+
+    for( int i = 0; i < 25; i++ ){
+        pixels[i] = 10*i;
+    }
+    image_info = AcquireMagickInfo( , "testimage"
+
+    MagickCoreGenesis( *argv, MagickTrue);
+    image = ConstituteImage( 5, 5, "I", IntegerPixel, &pixels, exception);
+
+    returnval = WriteImage( , image, exception)
+    */
+    
+    printf("test\n");
+    MagickWand *m_wand;
+    DrawingWand *d_wand;
+    PixelWand *c_wand;
+    unsigned long size;
+    int i;
+
+    size = 100;
+    double points[size];
+    for( i = 0; i < size; i++ ) {
+        points[i] = 255/i;
+    }
+
+    MagickWandGenesis();
+
+    m_wand = NewMagickWand();
+    d_wand = NewDrawingWand();
+    c_wand = NewPixelWand();
+
+    PixelSetColor( c_wand, "white" );
+    MagickNewImage( m_wand, size, size, c_wand );
+
+    DrawSetStrokeOpacity( d_wand, 1 );
+
+    for( i = 0; i < (size*size); i++ ) {
+        PixelSetGreen(c_wand, (double) i);
+        DrawSetFillColor(d_wand, c_wand);
+        DrawColor( d_wand, (double) i, (double) i, PointMethod );
+    }
+
+    MagickDrawImage(m_wand, d_wand);
+    MagickWriteImage(m_wand, "test.jpg");
+
+    c_wand = DestroyPixelWand(c_wand);
+    m_wand = DestroyMagickWand(m_wand);
+    d_wand = DestroyDrawingWand(d_wand);
+
+    MagickWandTerminus();
+    return 0;
+}
+
